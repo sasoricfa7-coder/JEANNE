1954474d37072fa08d1ca3c5c747727d571e4f37
marietou334 <chezmarietou@gmail.com>
2026-09-10 14:07:02 +0000

    Debut

---
+++ main.py
@@ -0,0 +1,15 @@
+# This file is part of JEANNE.
+# Copyright (C) 2026 Sasori
+# Licensed under the GNU AGPLv3-or-later. See the LICENSE file
+# at the project root.
+
+
+
+
+
+
+def main() :
+    print()
+
+if __name__=="__main__" :
+    main()
